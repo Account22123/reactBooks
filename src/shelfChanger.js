936cc1ab0bf6c5d85cd7b4ab50321d5d2316f90d
account22123 <donaldmallow@gmail.com>
2018-07-23 20:45:46 -0700

    Started to seperate search page from Main page

--- src/shelfChanger.js
+++ src/shelfChanger.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import './App.js'
+import BooksAPI from './BooksAPI.js'
+import { Link } from 'react-router-dom'
 
 class shelfChanger extends React.Component {
     render () {
-    return <div className='book-shelf-changer'>
+    return (
+    <div className='book-shelf-changer'>
         <select>
         <option value="move" disabled>Move to...</option>
         <option value="currentlyReading">Currently Reading</option>
@@ -12,6 +15,7 @@ class shelfChanger extends React.Component {
         <option value="none">None</option>
         </select>
         </div>
+        )
     }
 }
 
